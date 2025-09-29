bdc76ea5ad63fc4be93fa2aef4a19ed88cb6f33a
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:32:13 -0500

    Se realizo el tercer ejercicio

---
+++ 3eje.js
@@ -0,0 +1,7 @@
+let edad1 = prompt ("Ingrese la edad de la primera persona")
+let edad2 = prompt ("Ingrese la edad de la segunda persona")
+let edad3 = prompt ("Ingrese la edad de la tercera persona")
+let edad4 = prompt ("Ingrese la edad de la cuarta persona")
+
+let resultado = (parseInt(edad1)+parseInt(edad2)+parseInt(edad3)+parseInt(edad4))/4
+alert("El promedio de las edades es de: "+resultado)
