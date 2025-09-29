400d021c8ebea7a919070d05c855da91f5e88aa4
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:22:44 -0500

    Se realizo el primer ejercicio

---
+++ 1eje.js
@@ -0,0 +1,6 @@
+let nombre = prompt ("Ingrese el nombre del producto")
+let producto = prompt ("Ingrese la cantidad comprada del producto")
+let precio = prompt ("Ingrese el precio del producto")
+
+let resultado = producto*precio
+alert("El total a pagar por "+ producto +" de "+nombre+" es: "+resultado)
