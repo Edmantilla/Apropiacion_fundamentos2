e644f3e8e69326062a7ee37dda245521f55edd7a
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:44:57 -0500

    Se realizo el ejercicio 5

---
+++ 5eje.js
@@ -0,0 +1,7 @@
+let distancia = prompt ("Ingrese cuantos kilometros recorrio el auto")
+let velocidad = prompt ("Ingrese la velocidad del auto")
+
+let resultado = parseInt(distancia)/parseInt(velocidad)
+let resultadoF = resultado*60
+alert("el tiempo estimado del viaje es de: " +resultadoF+" minutos")
+
