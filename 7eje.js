15677f3b89a5d7255a3b63c55e72b4a4d80abf06
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:55:12 -0500

    Se realizo el ultimo ejercicio

---
+++ 7eje.js
@@ -0,0 +1,5 @@
+let nombre = prompt ("Ingrese el precio del producto")
+
+let resultado = nombre*0.10
+let tortal = nombre-resultado
+alert("El precio con el 10% de descuento del producto es de: "+tortal)
