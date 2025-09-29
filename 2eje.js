58c9976a4970629f60a11b7225ca6e22d3ab8fe6
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:27:01 -0500

    Se realizo el segundo ejercicio

---
+++ 2eje.js
@@ -0,0 +1,6 @@
+let horas = prompt ("Ingrese cuantas horas trabajo")
+let precio = prompt ("Ingrese el precio por hora")
+
+let resultado = (horas*precio)*7
+alert("El total a pagar por "+horas+" trabajadas durante una semana es de: "+resultado)
+
