7e305844c113b0f2a9172a421986804eba38f8ee
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:38:19 -0500

    Se realizo el 4 ejercicio

---
+++ 4eje.js
@@ -0,0 +1,5 @@
+let cantidad = prompt ("Ingrese cuantos kilometros recorrio")
+let consumo = prompt ("Ingrese la cantidad de gasolina consumida en litros")
+
+let resultado = parseInt(consumo)/parseInt(cantidad)
+alert("la cantidad consumida es de: "+resultado+" litros por kilometros recorrido")
