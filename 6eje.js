dbd020a3b4ad1e5c7d30478261806dc396025774
Serranomanuel <Serranomanuel106@gmail.com>
2025-09-29 17:52:34 -0500

    se realizo el 6 ejercicio

---
+++ 6eje.js
@@ -0,0 +1,6 @@
+let minutos = prompt ("Ingrese el numero de minutos que desea calcular")
+
+let hora = parseInt(minutos)/60
+let resultadoF = parseInt(minutos)%60
+alert("la convercion es de: "+ hora +" horas y "+resultadoF+" minutos")
+
